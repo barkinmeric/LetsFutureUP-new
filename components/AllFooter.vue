7a6d833eda27e7341613bac790a1551f7cb7f0a1
mustafasen <mustafasen@tuta.io>
2022-08-16 19:34:52 +0300

    footer-fix

--- components/AllFooter.vue
+++ components/AllFooter.vue
@@ -1,51 +1,53 @@
 <template>
 	<section class="footer">
-		<div id="footer_menu">
-			<div id="logo_wrapper">
-				<img src="/pages/index/lfu.png" />
+		<div class="footer-inner container">
+			<div id="footer_menu">
+				<div id="logo_wrapper">
+					<img src="/pages/index/lfu.png" />
+				</div>
+				<div id="who_r_we">
+					<p>{{ $t("nav.us.title") }}</p>
+					<ul>
+						<li v-for="(item, index) in $t('nav.us.content')" :key="item">
+							<NuxtLink :to="localePath(toUrl($t('nav.us.content', 'en')[index]))">
+								<h3 class="text">{{ item }}</h3>
+							</NuxtLink>
+						</li>
+					</ul>
+				</div>
+				<div id="what_v_do">
+					<p>{{ $t("nav.do.title") }}</p>
+					<ul>
+						<li v-for="(item, index) in $t('nav.do.content')" :key="item">
+							<NuxtLink :to="localePath(toUrl($t('nav.do.content', 'en')[index]))">
+								<h3 class="text">{{ item }}</h3>
+							</NuxtLink>
+						</li>
+					</ul>
+				</div>
+				<div id="hello_carr">
+					<p>{{ $t("nav.career.title") }}</p>
+					<ul>
+						<li v-for="(item, index) in $t('nav.career.content')" :key="item">
+							<NuxtLink :to="localePath(toUrl($t('nav.career.content', 'en')[index]))">
+								<h3 class="text">{{ item }}</h3>
+							</NuxtLink>
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div id="who_r_we">
-				<p>{{ $t("nav.us.title") }}</p>
-				<ul>
-					<li v-for="(item, index) in $t('nav.us.content')" :key="item">
-						<NuxtLink :to="localePath(toUrl($t('nav.us.content', 'en')[index]))">
-							<h3 class="text">{{ item }}</h3>
-						</NuxtLink>
-					</li>
-				</ul>
+			<div id="contact">
+				<span>&copy; Let’s FutureUP 2022. Tüm Hakları Saklıdır.</span>
+				<a href="mailto:humanresources@letsfutureup.com">humanresources@letsfutureup.com</a>
+				<span id="social_media">
+					<a href=""><img src="/pages/index/linkedin.svg" alt="" /></a>
+					<a href=""><img src="/pages/index/facebook.svg" alt="" /></a>
+					<a href=""><img src="/pages/index/instagram.svg" alt="" /></a>
+				</span>
 			</div>
-			<div id="what_v_do">
-				<p>{{ $t("nav.do.title") }}</p>
-				<ul>
-					<li v-for="(item, index) in $t('nav.do.content')" :key="item">
-						<NuxtLink :to="localePath(toUrl($t('nav.do.content', 'en')[index]))">
-							<h3 class="text">{{ item }}</h3>
-						</NuxtLink>
-					</li>
-				</ul>
-			</div>
-			<div id="hello_carr">
-				<p>{{ $t("nav.career.title") }}</p>
-				<ul>
-					<li v-for="(item, index) in $t('nav.career.content')" :key="item">
-						<NuxtLink :to="localePath(toUrl($t('nav.career.content', 'en')[index]))">
-							<h3 class="text">{{ item }}</h3>
-						</NuxtLink>
-					</li>
-				</ul>
-			</div>
-		</div>
-		<div id="contact">
-			<span>&copy; Let’s FutureUP 2022. Tüm Hakları Saklıdır.</span>
-			<a href="mailto:humanresources@letsfutureup.com">humanresources@letsfutureup.com</a>
-			<span id="social_media">
-				<a href=""><img src="/pages/index/linkedin.svg" alt="" /></a>
-				<a href=""><img src="/pages/index/facebook.svg" alt="" /></a>
-				<a href=""><img src="/pages/index/instagram.svg" alt="" /></a>
-			</span>
 		</div>
 	</section>
-</template>
+</template>	
 
 <script>
 export default {
@@ -69,69 +71,71 @@ export default {
 <style lang="scss" scoped>
 .footer {
 	background: $thirdColor;
-	list-style-type: none;
-	color: white;
-	#footer_menu {
-		display: grid;
-		#logo_wrapper {
-			grid-area: logoWrapper;
-		}
-		#hello_carr {
-			grid-area: helloCarr;
-		}
-		#who_r_we {
-			grid-area: whoRWe;
-		}
-		#what_v_do {
-			grid-area: whatVDo;
-		}
-		grid-template-areas: "logoWrapper helloCarr whoRWe whatVDo";
-		padding: 40px 5vw 20px 5vw;
-		gap: 40px;
-		@media (max-width: 800px) {
-			grid-template-areas:
-				"logoWrapper helloCarr"
-				"whoRWe whatVDo";
-			padding-right: 10vw;
-		}
-		p {
-			font-style: normal;
-			font-weight: 900;
-			font-size: 1rem;
-			line-height: 20px;
-			letter-spacing: 0.6px;
-		}
-		ul {
-			margin-top: 16px;
-			.text {
-				font-style: normal;
-				font-weight: normal;
-				font-size: 1rem;
-				line-height: 24px;
-				letter-spacing: 0.6px;
-				color: #ffffff;
+	.footer-inner {
+		list-style-type: none;
+		color: white;
+		#footer_menu {
+			display: grid;
+			#logo_wrapper {
+				grid-area: logoWrapper;
 			}
-		}
-	}
-	#contact {
-		display: flex;
-		flex-flow: row wrap;
-		justify-content: space-between;
-		align-items: center;
-		padding: 20px 5vw;
-		@media (max-width: 800px) {
-			flex-direction: column !important;
-		}
-		a {
-			text-decoration: none;
-			color: white;
-			margin-left: 15px;
+			#hello_carr {
+				grid-area: helloCarr;
+			}
+			#who_r_we {
+				grid-area: whoRWe;
+			}
+			#what_v_do {
+				grid-area: whatVDo;
+			}
+			grid-template-areas: "logoWrapper helloCarr whoRWe whatVDo";
+			padding: 40px 0 20px 0;
+			gap: 40px;
 			@media (max-width: 800px) {
-				margin-top: 15px;
-				margin-bottom: 15px;
+				grid-template-areas:
+					"logoWrapper helloCarr"
+					"whoRWe whatVDo";
+				padding-right: 10vw;
 			}
-			&visited {
+			p {
+				font-style: normal;
+				font-weight: 900;
+				font-size: 1rem;
+				line-height: 20px;
+				letter-spacing: 0.6px;
+			}
+			ul {
+				margin-top: 16px;
+				.text {
+					font-style: normal;
+					font-weight: normal;
+					font-size: 1rem;
+					line-height: 24px;
+					letter-spacing: 0.6px;
+					color: #ffffff;
+				}
+			}
+		}
+		#contact {
+			display: flex;
+			flex-flow: row wrap;
+			justify-content: space-between;
+			align-items: center;
+			padding: 20px 0;
+			@media (max-width: 800px) {
+				flex-direction: column !important;
+			}
+			a {
+				text-decoration: none;
 				color: white;
+				margin-left: 15px;
+				@media (max-width: 800px) {
+					margin-top: 15px;
+					margin-bottom: 15px;
+				}
+				&visited {
+					color: white;
+				}
 			}
 		}
 	}
