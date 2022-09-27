e3015feefc038fa28e1d388205a461e51c5d1b92
barkinmeric <barkinmeric@hotmail.com>
2022-09-28 00:42:56 +0300

    collapsible mentor mentee part is added

--- pages/mentorship-program.vue
+++ pages/mentorship-program.vue
@@ -6,6 +6,25 @@
 			<ment-grid :mentGrid="$t('mentorship.years.2021-2022.mentors')" />
 			<mentee-section />
 			<ment-grid :mentGrid="$t('mentorship.years.2021-2022.mentees')" />
+			<button @click="toggleCollapsible = !toggleCollapsible" :class="{ active: toggleCollapsible }" type="button" class="collapsible">
+				{{ $t(`mentorship.years.2020-2021.title`) }}
+			</button>
+			<div class="content" v-if="toggleCollapsible">
+				<div class="mentorship-collapsible-item">
+					<div class="container">
+						<div class="flex-container">
+							<ment-grid :mentGrid="$t('mentorship.years.2020-2021.mentors')" />
+						</div>
+					</div>
+				</div>
+				<div class="mentorship-collapsible-item">
+					<div class="container">
+						<div class="flex-container">
+							<ment-grid :mentGrid="$t('mentorship.years.2020-2021.mentees')" />
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</section>
 </template>
@@ -24,7 +43,67 @@ export default {
 			],
 		};
 	},
+	data() {
+		return {
+			toggleCollapsible: false,
+		};
+	},
 };
 </script>
 
-<style lang="scss" scope></style>
+<style lang="scss" scope>
+.mentorship-program {
+	.collapsible {
+		background-color: #eee;
+		color: #444;
+		cursor: pointer;
+		margin: 20px 0;
+		padding: 20px;
+		width: 100%;
+		text-align: left;
+		font-size: 15px;
+		line-height: 30px;
+		text-transform: uppercase;
+		border-radius: 8px;
+		&:after {
+			content: "\002B";
+			color: black;
+			font-size: 30px;
+			font-weight: bold;
+			float: right;
+			margin-left: 5px;
+		}
+		&:hover {
+			background-color: #ccc;
+		}
+	}
+	.active {
+		background-color: #ccc;
+		&:after {
+			content: "\2212";
+		}
+	}
+	.content {
+		padding: 0 18px;
+		overflow: hidden;
+		background-color: #f1f1f1;
+	}
+	.mentorship-collapsible-item {
+		text-align: center;
+		.title {
+			display: inline-block;
+			padding-top: 15px;
+			color: black;
+			font-weight: 500;
+			font-size: 22px;
+			border-bottom: 2px solid $sideColor;
+			text-transform: uppercase;
+		}
+		.flex-container {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: center;
+		}
+	}
+}
+</style>
